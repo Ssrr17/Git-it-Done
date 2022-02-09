c151e6caa28949211caf947b7a95bdca1c0fe9bf
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-09 13:50:35 -0500

    API now dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,13 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
-};
-getUserRepos()
+var getUserRepos = function (user) {
+    //format github api url
+    var apiUrl = "https://api.github.com/users/" + user +"/repos";
+
+    //make a response to the URL
+
+    fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+    console.log(data);
+});
+});
+}
+getUserRepos("facebook");
